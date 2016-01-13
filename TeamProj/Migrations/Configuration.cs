dfc41e64c85ab562d6ae4d06968a141c0c6c2ac3
VadimVlasov <vadim_vl96@mail.ru>
2016-01-13 13:09:45 +0300

    three

--- TeamProj/Migrations/Configuration.cs
+++ TeamProj/Migrations/Configuration.cs
@@ -14,6 +14,23 @@ namespace TeamProj.Migrations
 
         protected override void Seed(TeamProj.Context context)
         {
+            context.Salesmen.AddOrUpdate(
+                p=>p.name,
+                new Salesman { name="Amazon"},
+                new Salesman { name="Apple"},
+                new Salesman { name="Samsung"}
+                );
+            
+           var sale1=context.Salesmen.Select(a=>a.name=="Amazon");
+            var sale2=context.Salesmen.Where(a=>a.name=="Apple");
+            var sale3=context.Salesmen.Where(a=>a.name=="Samsung");
+
+            context.Good.AddOrUpdate(
+                new Goods { name ="IPhone", salesman= context.Salesmen.Where(a=>a.name=="Apple").FirstOrDefault() },
+                new Goods { name = "IPad", salesman = context.Salesmen.Where(a => a.name == "Apple").FirstOrDefault() },
+                new Goods { name = "Galaxy S6", salesman = context.Salesmen.Where(a => a.name == "Samsung").FirstOrDefault() },
+                new Goods { name = "Kindle", salesman = context.Salesmen.Where(a => a.name == "Amazon").FirstOrDefault() }                
+                );
             //  This method will be called after migrating to the latest version.
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
