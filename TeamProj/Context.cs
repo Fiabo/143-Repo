2c9d715262cb0f67131b1b3fe0864c478a7e9608
VadimVlasov <vadim_vl96@mail.ru>
2016-01-13 12:55:37 +0300

    Secondnew

---
+++ TeamProj/Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace TeamProj
+{
+    class Context : DbContext
+    {
+        public DbSet<Salesman> Salesmen { get; set; }
+        public DbSet<Goods> Good { get; set; }
+
+        public Context():
+            base("TeamBDOnline")
+        {
+                    
+        }
+    }
+}
